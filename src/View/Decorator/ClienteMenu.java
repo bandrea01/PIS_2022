39c15984efd91b36e6aba657aea0272c531fff14
Andrea Barone <andrea.barone1401@gmail.com>
2022-09-11 20:21:23 +0200

    7th commit 11/09 Creata UI admin Creata UI admin per gestione punti
    Modifiche al DAO per metodi utili
    
    Business per Punti vendita
    
    Cancellazione classi di prova durante lezioni

---
+++ src/View/Decorator/ClienteMenu.java
@@ -0,0 +1,31 @@
+package View.Decorator;
+
+import View.MainLayout;
+import View.ViewModel.ButtonCreator;
+
+import javax.swing.*;
+import java.util.List;
+
+
+public class ClienteMenu extends ClienteMenuDecorator {
+
+    public ClienteMenu(Menu menu, MainLayout window) {
+        this.menu = menu;
+        this.window = window;
+    }
+
+    @Override
+    public List<JButton> getButtons() {
+        //Funzioni guest
+        buttons.addAll(this.menu.getButtons());
+        //Funzioni cliente
+        JButton order = ButtonCreator.createButton("My Orders", true, 100,149,237, e -> window.showOrders(), null);
+        JButton profile = ButtonCreator.createButton("My Profile", true, 100,149,237, e -> window.showProfile(), null);
+
+        buttons.add(order);
+        buttons.add(profile);
+
+        return buttons;
+    }
+}
+
